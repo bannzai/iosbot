7e2532030ee3e2c1ade5602450ef562908da64e4
bannzai <kingkong999yhirose@gmail.com>
2018-11-20 18:48:23 +0900

    :sparkles: Using toml of config

--- config.go
+++ config.go
@@ -1,15 +1,16 @@
 package main
 
 type Config struct {
-	BotToken              string `requiered:"true" envconfig:"BOT_TOKEN"`
-	BotID                 string `requiered:"true" envconfig:"BOT_ID"`
-	ChannelID             string `requiered:"true" envconfig:"CHANNEL_ID"`
-	DebugChannelID        string `requiered:"true" envconfig:"DEBUG_CHANNEL_ID"`
-	GitHubUsername        string `requiered:"true" envconfig:"GITHUB_USERNAME"`
-	GitHubToken           string `requiered:"true" envconfig:"GITHUB_TOKEN"`
-	GitHubRepositoryOwner string `requiered:"true" envconfig:"GITHUB_REPOSITORY_OWNER"`
-	GitHubRepositoryName  string `requiered:"true" envconfig:"GITHUB_REPOSITORY_NAME"`
-	GitCommitAuthorName   string `requiered:"true" envconfig:"GIT_COMMIT_AUTHOR_NAME"`
-	GitCommitAuthorEmail  string `requiered:"true" envconfig:"GIT_COMMIT_AUTHOR_EMAIL"`
-	InfoPlistPath         string `requiered:"true" envconfig:"INFOPLIST_PATH"`
+	BotToken              string `toml:"bot_token"`
+	VerificationToken     string `toml:"verification_token"`
+	BotID                 string `toml:"bot_id"`
+	ChannelID             string `toml:"channel_id"`
+	DebugChannelID        string `toml:"debug_channel_id"`
+	GitHubUsername        string `toml:"github_username"`
+	GitHubToken           string `toml:"github_token"`
+	GitHubRepositoryOwner string `toml:"github_repository_owner"`
+	GitHubRepositoryName  string `toml:"github_repository_name"`
+	GitCommitAuthorName   string `toml:"github_commit_author_name"`
+	GitCommitAuthorEmail  string `toml:"github_commit_author_email"`
+	InfoPlistPath         string `toml:"infoplist_path"`
 }
